ec3134e2a9838f47d6029bbdfeb3b09254162690
unknown <nurlanemenov14@gmail.com>
2024-08-29 00:52:31 +0400

    first commit

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const mehsul = [
             "img/mehsul2.png",
         ],
         model: "SKU: 61346969134",
-        name: "Mercedes üçün alışdırmaşamları",
+        name: "Cigarette Lighter",
         price: 18.29,
         currency: "AZN",
         stock: "Stokda var"
@@ -113,7 +113,9 @@ const mehsul = [
 
 const mehsularcontainer = document.getElementById('mehsularcontainer');
 let mehsulcard = document.querySelector(".mehsulcard")
-for (let i = 0; i < mehsul.length; i++) {
+let likes=document.getElementById("likes")
+
+for (let i =0; i < mehsul.length; i++) {
     const borderrengim = mehsul[i].stock === "Stokda var" ?"#5BC904" : "#FF0000";
     const stockIcon = mehsul[i].stock === "Stokda var"
         ? '<i class="fa-solid fa-circle-check"></i>'
@@ -121,19 +123,22 @@ for (let i = 0; i < mehsul.length; i++) {
 
     mehsularcontainer.innerHTML += `
         <div class="mehsulcard">
+       
           <div class="mehsul-img">
             <img src="${mehsul[i].images[0]}" alt="mehsul">
           </div>
           <div class="mehsular-text">
             <h6>${mehsul[i].model}</h6>
-            <h4>${mehsul[i].name}</h4>
+            <h5>${mehsul[i].name}</h5>
             <div class="stok ">${stockIcon} <span style="color: ${mehsul[i].stock === "Stokda var" ?"#5BC904" : "#FF0000"}">${mehsul[i].stock}</span></div>
             <p>${mehsul[i].price} ${mehsul[i].currency}</p>
             <button class="sebetat2"> <i class="fa-solid fa-cart-shopping"></i>Səbətə əlavə et</button>
           </div>
         </div>`
-    
+   
     }
+  
+    
 
         function toggleMenu() {
             const menu = document.querySelector(".menu-bars");
