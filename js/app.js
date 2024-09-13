60f623baaa0061d92fdb52cd44827188ce56e52a
unknown <nurlanemenov14@gmail.com>
2024-09-13 22:01:11 +0400

    first commit

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
-const marka=[
+const marka = [
     {
-        modeller:"BMW",
+        modeller: "BMW",
     },
     {
-        modeller:"HYUNDAI",
+        modeller: "HYUNDAI",
     },
     {
-        modeller:"INFINITI",
+        modeller: "INFINITI",
     },
     {
-        modeller:"JAGUAR",
+        modeller: "JAGUAR",
     },
     {
-        modeller:"KIA",
+        modeller: "KIA",
     },
     {
-        modeller:"LAND ROVER",
+        modeller: "LAND ROVER",
     },
     {
-        modeller:"LEXUS",
+        modeller: "LEXUS",
     },
     {
-        modeller:"MASERATI",
+        modeller: "MASERATI",
     },
     {
-        modeller:"MERCEDES BENZ",
+        modeller: "MERCEDES BENZ",
     },
     {
-        modeller:"MINI",
+        modeller: "MINI",
     },
 ]
 
@@ -40,10 +40,10 @@ const mehsul = [
         model: "SKU: 61346969134",
         name: "Cigarette Lighter",
         price: 279.29,
-     
+
         currency: "AZN",
         stock: "Stokda var"
-        
+
     },
     {
         images: [
@@ -76,7 +76,7 @@ const mehsul = [
         name: "Cigarette Lighter",
         price: 18.29,
         currency: "AZN",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -87,7 +87,7 @@ const mehsul = [
         name: "Cigarette Lighter",
         price: 279.29,
         currency: "AZN",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -118,7 +118,7 @@ const mehsul = [
         ],
         model: "SKU: 61346969134",
         name: "Cigarette Lighter",
-        price:279.29,
+        price: 279.29,
         currency: "AZN",
         stock: "Stokda var"
     },
@@ -132,7 +132,7 @@ const mehsul = [
         price: 18.29,
         currency: "AZN",
         stock: "Stokda yoxdu",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -144,7 +144,7 @@ const mehsul = [
         price: 18.29,
         currency: "AZN",
         stock: "Stokda yoxdu",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -156,7 +156,7 @@ const mehsul = [
         price: 18.29,
         currency: "AZN",
         stock: "Stokda yoxdu",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -168,7 +168,7 @@ const mehsul = [
         price: 18.29,
         currency: "AZN",
         stock: "Stokda yoxdu",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -180,7 +180,7 @@ const mehsul = [
         price: 18.29,
         currency: "AZN",
         stock: "Stokda yoxdu",
-        stock: "Yoldadır" 
+        stock: "Yoldadır"
     },
     {
         images: [
@@ -189,10 +189,10 @@ const mehsul = [
         ],
         model: "SKU: 61346969134",
         name: "Cigarette Lighter",
-        price: 279.29 ,
+        price: 279.29,
         currency: "AZN",
         stock: "Stokda var",
-      
+
     }
 ];
 
@@ -202,50 +202,62 @@ const mehsularcontainer = document.getElementById('mehsularcontainer');
 const mehsularcontainer2 = document.getElementById('mehsularcontainer2');
 let mehsulcard = document.querySelector(".mehsulcard")
 let mehsulcard2 = document.querySelector(".mehsulcard2")
-let like=document.getElementById("like")
-const markalar=document.getElementById("markalar")
-const markalar2=document.getElementById("markalar2")
+let like = document.getElementById("like")
+const markalar = document.getElementById("markalar")
+const markalar2 = document.getElementById("markalar2")
+const kabinet = document.getElementById('kabinet')
 
-const markaad= new Set()
-const markaad2= new Set()
 
-for(let j=0;j<marka.length;j++){
+function kabinetgo(){
+    if(kabinet.style.display==="none"||kabinet.style.display==="" ){
+        kabinet.style.display="flex"
+    }else{
+       kabinet.style.display="none"
+    }
+}
+
+const markaad = new Set()
+const markaad2 = new Set()
+
+
+
+for (let j = 0; j < marka.length; j++) {
     markaad.add(marka[j].modeller)
     markaad2.add(marka[j].modeller)
 }
 
-const elavemarka=Array.from(markaad)
+const elavemarka = Array.from(markaad)
 
-function markaadd(){
- markalar.innerHTML=''
- markalar2.innerHTML=''
-    for (let j = 0; j < elavemarka.length; j++){
-       
-        markalar.innerHTML+=`<option>${elavemarka[j]}</option>`
-        markalar2.innerHTML+=`<option>${elavemarka[j]}</option>`
+function markaadd() {
+    markalar.innerHTML = ''
+    markalar2.innerHTML = ''
+    for (let j = 0; j < elavemarka.length; j++) {
+
+        markalar.innerHTML += `<option>${elavemarka[j]}</option>`
+        markalar2.innerHTML += `<option>${elavemarka[j]}</option>`
     }
 }
 markaadd()
 
 
-for (let i = 0; i < mehsul.length  ; i++) {
-    const borderrengim = mehsul[i].stock === "Stokda var" ? "#5BC904" :'#DF2127';
+for (let i = 0; i < mehsul.length; i++) {
+    const borderrengim = mehsul[i].stock === "Stokda var" ? "#5BC904" : '#DF2127';
     const stockcolor = mehsul[i].stock === "Yoldadır" ? "#FFA115" : "#DF2127";
-    const stockIcon = mehsul[i].stock === "Stokda var" 
+    const stockIcon = mehsul[i].stock === "Stokda var"
         ? '<i class="fa-solid fa-circle-check"></i>'
         : '<i style="color:#DF2127" class="fa-solid fa-circle-xmark stokyox"></i>';
 
     mehsularcontainer.innerHTML += `
-         <div  onclick="gopage()" href="page/single.htm" data-aos="fade-right" class="mehsulcard">
-          <div class="mehsul-img">
+         <div  data-aos="fade-right" class="mehsulcard">
+          <div  class="mehsul-img">
             <div class="like">
               <div class="like-content">
                 <i class="fa-solid fa-heart"></i>
               </div>
             </div>
-            <img src="${mehsul[i].images[0]}" alt="mehsul">
+            <img   src="${mehsul[i].images[0]}" alt="mehsul">
           </div>
-          <div class="mehsular-text">
+          <div onclick="gopage()"  class="mehsular-text">
             <h6>${mehsul[i].model}</h6>
             <h5>${mehsul[i].name}</h5>
             <div class="stok">${stockIcon} <span style="color: ${borderrengim}">${mehsul[i].stock}</span></div>
@@ -264,7 +276,7 @@ for (let i = 0; i < mehsul.length  ; i++) {
             </div>
             <img src="${mehsul[i].images[0]}" alt="mehsul">
           </div>
-          <div class="mehsular-text">
+          <div onclick="gopage()"  class="mehsular-text">
             <h6>${mehsul[i].model}</h6>
             <h5>${mehsul[i].name}</h5>
             <div class="stok">${stockIcon} <span style="color: ${borderrengim}">${mehsul[i].stock}</span></div>
@@ -273,42 +285,44 @@ for (let i = 0; i < mehsul.length  ; i++) {
             <button class="sebetat2mobile"> <i class="fa-solid fa-cart-shopping"></i></button>
           </div>
         </div>`;
-         
+
 }
 
 const likeIcons = document.querySelectorAll('.like-content i');
 
 likeIcons.forEach(icon => {
-    icon.addEventListener('click', function() {
-     
+    icon.addEventListener('click', function () {
+
         this.style.color = this.style.color === 'rgb(255, 0, 0)' ? '#DFDFDF' : '#FF0000';
     });
 });
 
-        function toggleMenu() {
-            const menu = document.querySelector(".menu-bars");
-            menu.classList.toggle("open");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-bars");
+    menu.classList.toggle("open");
+}
+
+function startCountdown(duration) {
+    let timer = duration, hours, minutes, seconds;
+
+    setInterval(function () {
+        hours = Math.floor(timer / 3600);
+        minutes = Math.floor((timer % 3600) / 60);
+        seconds = Math.floor(timer % 60);
+
+        document.getElementById('hours').textContent = hours + " saat";
+        document.getElementById('minutes').textContent = minutes + " Dəqiqə";
+        document.getElementById('seconds').textContent = seconds + " Saniyə";
+
+        if (--timer < 0) {
+            timer = duration;
         }
-    
-        function startCountdown(duration) {
-            let timer = duration, hours, minutes, seconds;
-        
-            setInterval(function () {
-                hours = Math.floor(timer / 3600);
-                minutes = Math.floor((timer % 3600) / 60);
-                seconds = Math.floor(timer % 60);
-        
-                document.getElementById('hours').textContent = hours + " saat";
-                document.getElementById('minutes').textContent = minutes + " Dəqiqə";
-                document.getElementById('seconds').textContent = seconds + " Saniyə";
-        
-                if (--timer < 0) {
-                    timer = duration; 
-                }
-            }, 1000);
-        }
-        
-        startCountdown(10800);
-        function gopage(){
-            window.location.href='../page/single.htm'
-        }
+    }, 1000);
+}
+
+startCountdown(10800);
+function gopage() {
+    window.location.href = '../page/single.htm'
+}
+
+
