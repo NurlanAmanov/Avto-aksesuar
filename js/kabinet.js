4cf0c5b617496151fca600c8d5ecc3b723f7a0a2
unknown <nurlanemenov14@gmail.com>
2024-09-19 14:45:19 +0400

    first commit

--- js/kabinet.js
+++ js/kabinet.js
@@ -9,4 +9,24 @@ function closreset(){
     if(resetle.style.display==='block'){
         resetle.style.display="none"
     } 
+    
+}
+
+function profduzelis(){
+    let profiduzlt = document.getElementById('profiduzlt');
+    let unvankitab = document.getElementById('unvankitab'); // unvankitab elementini əldə edirik
+    if(profiduzlt.style.display === 'flex'){
+        profiduzlt.style.display = 'none'; // Bağlayır
+    } else {
+        profiduzlt.style.display = 'flex'; // Açır
+        unvankitab.style.display = 'none'; // Digər elementi gizlədir
+    }
+}
+
+function openumav(){
+    let unvankitab=document.getElementById('unvankitab')
+    if(unvankitab.style.display==='none'  || unvankitab.style.display===''){
+        unvankitab.style.display='flex' ,
+         profiduzlt.style.display='none'
+    }
 }
